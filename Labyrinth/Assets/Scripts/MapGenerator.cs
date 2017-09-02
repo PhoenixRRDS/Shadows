e89234f084501f94651ff76911593340076ae71a
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-09-02 19:47:23 +0530

    Noise Map: Generate Noise Map
    Generate Noise map using perling noise values
    and render them to a plane
    
    Noise.cs: Create a Noise map matrix
    Map Display: DrawNoiseMap:  Takes a 2D matrix as input and applies the
    perlin noise data on a given Renderer
    MapGenerator: GenerateMap: Creates a noiseMap using Noise.GenerateMap
    and applies the display using display.DrawNoiseMap()
    MapGeneratorEditor: An editor script to create a button that will call
    MapGenerator.GenerateMap()
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

---
+++ Labyrinth/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * MapGenerator.cs
+ * Copyright (c) 2017 Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
+ * AUTHORS
+ * Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
---
+++ Labyrinth/Assets/Scripts/Noise.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Noise.cs
+ * Copyright (c) 2017 Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
+ * AUTHORS
+ * Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise {
+
+    #region Variables
+    #endregion
+
+    #region Unity Methods
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        if (scale == 0.0f) {
+            scale = 0.0001f;
+        }
+
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+    #endregion
+}
