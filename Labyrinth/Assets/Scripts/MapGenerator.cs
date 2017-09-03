b302e0d819af7cef703aa050a2c8a28178d57213
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-09-03 14:18:33 +0530

    Noise: Generate map as combination of octaves
    The resultant noise is a combination of a number of octaves, whose
    frequency and amplitude is determined by the lacuranity and persistance
    provided by the user respectively. Generally, the frequency and
    amplitude change via:
    
      frequency of an octave = lacuranity ^ octave_no
      amplitude of an octave = persistance ^ octave_no
    
    The resultant map is a combination of all the octaves created in this
    process.
    
    Each octave is generated via an octave offset, which is generated pseudo
    randomly depending on the value of a "seed" provided, so that we can get
    a specific map using a specific seed.
    
    "halfWidth" and "halfHeight" is used to determine the mid poin position
    of the map, so that the "zooming" while moving the noiseScale slider is
    done along the center and not along the top right corner.
    
    The noiseMap[,] is clamped in the range of [0,1] later using
    Mathf.InverseLerp().
    
    Finally, in the MapGenerator.cs, we provide a Tooltip for few of the
    public variables, and limit it's value using the "OnValidate()" method.
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- Labyrinth/Assets/Scripts/MapGenerator.cs
+++ Labyrinth/Assets/Scripts/MapGenerator.cs
@@ -14,13 +14,41 @@ public class MapGenerator : MonoBehaviour {
     public int mapHeight;
     public float noiseScale;
 
+    [Tooltip("Number of noise maps, addition of these will form the resultant map")]
+    public int octaves;
+    [Range(0, 1)]
+    [Tooltip("Persistance controls decrease in amplitude of octaves. Amplitude of an octave = persistance ^ octave_no")]
+    public float persistance;
+    [Tooltip("Lacunarity controls the increase in frequency of octaves. Frequency of an octave = lacunarity ^ octave_no")]
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1) {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1) {
+            mapHeight = 1;
+        }
+
+        if (lacunarity < 1) {
+            lacunarity = 1;
+        }
+        if (octaves < 0) {
+            octaves = 0;
+        }
     }
 }
